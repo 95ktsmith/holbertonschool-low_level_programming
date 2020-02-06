4f4df218f74a90efbcf71cd81ca9e84d84a929d8
95ktsmith <95ktsmith@gmail.com>
2020-02-06 18:41:47 +0000

    Corrected file name.

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: Prints every different combination of two digits
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int k = 0;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = k; j < 10; j++)
+		{
+			putchar (i + '0');
+			putchar (j + '0');
+			if (i != 9 || j != 9)
+			{
+				putchar (',');
+				putchar (' ');
+			}
+		}
+		k++;
+	}
+	putchar ('\n');
+	return (0);
+}
