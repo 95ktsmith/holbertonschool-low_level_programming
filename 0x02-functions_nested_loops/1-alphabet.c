9a361484877e163061571cf6ccd753da3e13f283
95ktsmith <95ktsmith@gmail.com>
2020-02-10 18:47:03 +0000

    Initial commit.

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet - Prints alphabet
+ * Description: Prints alphabet in lowercase, followed by new line.
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+	_putchar('\n');
+}
