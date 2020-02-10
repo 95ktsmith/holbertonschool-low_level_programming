4f33193e0293117c3b096e9fc28c644e02557536
95ktsmith <95ktsmith@gmail.com>
2020-02-10 20:49:22 +0000

    Initial commit.

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * add - Adds two integers
+ * @a: First number to be added
+ * @b: Second number to be added
+ * Description: Adds two integers
+ * Return: Sum of the integers
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
