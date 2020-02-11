4c0c94e5b5d4f47f6d245bc85af536a66f0d9d3c
95ktsmith <95ktsmith@gmail.com>
2020-02-11 20:23:39 +0000

    Corrected largest_number prototype.

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -2,6 +2,9 @@
 #define _HOLBERTON_H_
 
 void _putchar(char);
+
 int positive_or_negative(int);
-int largest_number(int, int, int);
+
+int largest_number(int a, int b, int c);
+
 #endif /* _HOLBERTON_H_ */
