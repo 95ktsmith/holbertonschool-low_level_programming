d4f1d45661d5f70f892032488c1a5cb3a0d2a691
95ktsmith <95ktsmith@gmail.com>
2020-02-20 22:48:19 +0000

    Initial commit.

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * rot13 - Rot13 encryption
+ * Description: Puts a string through Rot13 encryption
+ * @str: String to be processed
+ * Return: Pointer to string
+ */
+
+char *rot13(char *str)
+{
+	return (str);
+}
