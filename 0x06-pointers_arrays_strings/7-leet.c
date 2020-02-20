ea9e0ed0555091bb3c85f8b27648930f67244ff3
95ktsmith <95ktsmith@gmail.com>
2020-02-20 23:05:50 +0000

    Added code.

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,5 +9,21 @@
 
 char *leet(char *str)
 {
+	char letters[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	char numbers[] = {'4', '4', '3', '3', 'o', 'o', '7', '7', '1', '1'};
+	int strindex;
+	int leetindex;
+
+	for (strindex = 0; str[strindex] != '\0'; strindex++)
+	{
+		for (leetindex = 0; leetindex < 10; leetindex++)
+		{
+			if (str[strindex] == letters[leetindex])
+			{
+				str[strindex] -= (str[strindex] -
+						  numbers[leetindex]);
+			}
+		}
+	}
 	return (str);
 }
