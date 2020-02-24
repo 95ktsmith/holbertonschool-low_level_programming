3b4e32c583ba136ae99c3f324f748c75d337a382
95ktsmith <95ktsmith@gmail.com>
2020-02-24 19:40:32 +0000

    Initial commit.

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * _strpbrk - String point break
+ * Description: Finds the first occurance in string s of any characters from
+ *              string accept.
+ * @s: String to look through
+ * @accept: Characters to look for
+ * Return: Pointer to first occurance
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int sindex, aindex;
+	int done = 0;
+
+	for (sindex = 0; *(s + sindex) != '\0' && done == 0; sindex++)
+	{
+		for (aindex = 0; *(accept + aindex) != '\0'; aindex++)
+		{
+			if (*(s + sindex) == *(accept + aindex))
+			{
+				done = 1;
+				break;
+			}
+		}
+	}
+	return (s + sindex - 1);
+}
