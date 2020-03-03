0775e8de54cb5f1f1969f68a3396b0cf4b3da0d7
95ktsmith <95ktsmith@gmail.com>
2020-03-03 17:06:29 +0000

    Added NULL returns for fail conditions.

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,7 +14,13 @@ char *create_array(unsigned int size, char c)
 	char *array;
 	unsigned int index;
 
+	if (size == 0)
+		return (NULL);
+
 	array = malloc(size);
+	if (array == NULL)
+		return (NULL);
+
 	for (index = 0; index < size; index++)
 		*(array + index) = c;
 	return (array);
