64d7d363b7cf37c9682f3d070801b32c58d09b89
Kevin Smith <95ktsmith@gmail.com>
2020-03-24 12:11:01 +0000

    Only set head to NULL at the end, rather than after freeing every node.

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,5 +1,7 @@
 #include "lists.h"
 
+void free_rec(listint_t *head);
+
 /**
  * free_listint2 - free listint_t
  * Description: Frees a listint_t, sets the head to NULL
@@ -8,10 +10,21 @@
 
 void free_listint2(listint_t **head)
 {
-	if (!(*head))
-		return;
-	if ((*head)->next)
-		free_listint2(&((*head)->next));
-	free(*head);
+	free_rec(*head);
 	*head = NULL;
+}
+
+/**
+ * free_rec - free recursively
+ * Description: Frees a list
+ * @head: Head of the list
+ */
+
+void free_rec(listint_t *head)
+{
+	if (!head)
+		return;
+	if (head->next)
+		free_rec(head->next);
+	free (head);
 }
