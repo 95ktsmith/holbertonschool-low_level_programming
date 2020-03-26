b71f0d9b8c80f6e6debc96b99bb39c2d4c6c5ac3
95ktsmith <95ktsmith@gmail.com>
2020-03-26 18:51:12 +0000

    Reduced bound to 32 bits instead of 64.

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -14,7 +14,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	unsigned long int bits = 0;
 	int index;
 
-	for (index = 0; index < 64; index++)
+	for (index = 0; index < 32; index++)
 		bits += ((n ^ m) & 1 << index ? 1 : 0);
-	return (bits >> 1);
+	return (bits);
 }
