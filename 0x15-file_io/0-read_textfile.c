3343a7b54ff9905c4a99d3d4fe998787f1cde57a
Kevin Smith <95ktsmith@gmail.com>
2020-03-31 16:55:00 +0000

    Added return if file failed to open.

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,7 +12,7 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int file;
-	ssize_t read_bytes;
+	ssize_t read_bytes, written_bytes;
 	char *buffer;
 
 	if (!filename || letters == 0)
@@ -23,21 +23,23 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	file = open(filename, O_RDONLY);
+	if (file < 0)
+	{
+		free(buffer);
+		return (0);
+	}
+
 	read_bytes = read(file, buffer, letters);
 	if (read_bytes == -1)
 	{
+		close(file);
 		free(buffer);
 		return (0);
 	}
 
-	if (write(1, buffer, read_bytes) != read_bytes)
-	{
-		free(buffer);
-		close(file);
-		return (0);
-	}
+	written_bytes = write(1, buffer, read_bytes);
 
 	free(buffer);
 	close(file);
-	return (read_bytes);
+	return ((written_bytes == read_bytes ? written_bytes : 0));
 }
