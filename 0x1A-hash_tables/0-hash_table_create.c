f4409a0bd4fc0c53e0cf3b5d1a5fa8bf072161b2
Kevin Smith <95ktsmith@gmail.com>
2020-05-29 14:46:16 +0000

    Set hash_table size before returning.

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,14 +12,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table = malloc(sizeof(hash_table_t));
 	if (hash_table == NULL)
 		return (NULL);
-	if (size == 0)
-		return (hash_table);
 	hash_table->array = malloc(sizeof(hash_node_t *) * size);
 	if (hash_table->array == NULL)
 	{
 		free(hash_table);
 		return (NULL);
 	}
-
+	hash_table->size = size;
 	return (hash_table);
 }
