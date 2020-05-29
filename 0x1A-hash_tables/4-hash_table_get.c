4df6ca5b726d1a9a22c41ec988b0a687f83f6739
Kevin Smith <95ktsmith@gmail.com>
2020-05-29 16:18:08 +0000

    Initial commit.

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,21 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - Get the value associated with a key in a hash table
+ * @ht: Pointer to hash table
+ * @key: Key to look for
+ * Return: Pointer to value if successful, NULL otherwise
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	int index;
+
+	if (ht == NULL || key == NULL)
+		return (NULL);
+
+	index = hash_djb2((const unsigned char *)key) % ht->size;
+	if (ht->array[index] == NULL)
+		return (NULL);
+
+	return (ht->array[index]->value);
+}
